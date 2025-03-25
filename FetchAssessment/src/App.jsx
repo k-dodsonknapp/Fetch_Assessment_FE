90336dad88363d057a56487eb805f29a7b6c6498
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2025-03-24 22:59:59 -0700

    added shadcn and other utilities

--- FetchAssessment/src/App.jsx
+++ FetchAssessment/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
